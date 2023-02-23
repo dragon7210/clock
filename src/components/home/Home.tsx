f56c86455dfcb4bcf32a96615431b2e171275dba
dragon <isanoguchi721@gmail.com>
2023-02-23 07:58:34 -0500

    fix

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState, useRef } from "react";
 
 const Home = () => {
-  const [time, setTime] = useState<string>("");
+  const [sec, setSec] = useState<number>();
+  const [min, setMin] = useState<number>();
+  const [hour, setHour] = useState<number>();
   const [count, setCount] = useState<number>(0);
   const ref = useRef(0);
   ref.current = count;
@@ -13,9 +15,16 @@ const Home = () => {
   const updateTime = () => {
     setCount(ref.current + 1);
   };
-  console.log(count);
+  useEffect(() => {
+    setSec(count % 60);
+    setMin(+(count / 60).toFixed(0));
+    setHour(+(count / 3600).toFixed(0));
+  }, [count]);
   return (
     <div className="flex justify-center">
+      <p className="text-[40px] text-[green] font-extrabold ml-[20px]">
+        {hour}:{min}:{sec}
+      </p>
       <p className="text-[40px] text-[green] font-extrabold ml-[20px]">
         {count}
       </p>
