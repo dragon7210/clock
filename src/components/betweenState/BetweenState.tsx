ec8ddb601d48c31b94dcfe97d99b11dc4af4081c
dragon <isanoguchi721@gmail.com>
2023-02-27 10:23:51 -0500

    fix

--- src/components/betweenState/BetweenState.tsx
+++ src/components/betweenState/BetweenState.tsx
@@ -5,14 +5,7 @@ type Props = {
 
 const BetweenState = ({ state, color }: Props) => {
   return (
-    <p
-      className={
-        `text-[50px] font-extrabold ` +
-        ((color === "green" && "text-[green]") ||
-          (color === "blue" && "text-[blue]") ||
-          (color === "red" && "text-[red]"))
-      }
-    >
+    <p className="text-[50px] font-extrabold" style={{ color: color }}>
       {state ? ":" : " "}
     </p>
   );
--- src/components/numberFormat/NumberFormat.tsx
+++ src/components/numberFormat/NumberFormat.tsx
@@ -6,12 +6,8 @@ type Props = {
 const NumberFormat = ({ value, color }: Props) => {
   return (
     <p
-      className={
-        `text-[50px] font-extrabold w-[70px] caret-transparent ` +
-        ((color === "red" && "text-[red]") ||
-          (color === "blue" && "text-[blue]") ||
-          (color === "black" && "text-[black]"))
-      }
+      className="text-[50px] font-extrabold w-[70px] caret-transparent"
+      style={{ color: color }}
     >
       {value > 9 ? value : "0" + value}
     </p>
